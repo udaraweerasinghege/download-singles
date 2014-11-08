58395029be19b556d724972082361f1567fd18b7
Udara Weerasinghege <udara.weerasinghege@mail.utoronto.ca>
2014-11-08 02:19:00 -0500

    Let's use APIs!

--- getSongLastFm.py
+++ getSongLastFm.py
@@ -5,12 +5,14 @@ import json
 API_KEY = '5b905641511ae41eee7001a79e88775f'
 SECRET = 'cf537d2466ff4f57cabdf762db1bf980'
 
-def get_top_10(artist):
-    response = requests.get('http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist\
-    ={0}&autocorrect=1&limit=10&api_key=5b905641511ae41eee7001a79e88775f&format=json'.format(artist))
+def get_song(artist):
+    response = requests.get('http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist={0}\
+    &autocorrect=1&limit=10&api_key=5b905641511ae41eee7001a79e88775f&format=json'.format(artist))
     data = response.json()
 
 
     track_list = data['toptracks']['track']
     for track in track_list:
         print(track['name'])
+
+get_song('ladygaga')
