c3d490b895c6b1a7041e0df9677a7d9c209dbff0
Udara Weerasinghege <udara.weerasinghege@mail.utoronto.ca>
2014-11-08 02:25:20 -0500

    Everything works

--- getSongLastFm.py
+++ getSongLastFm.py
@@ -1,6 +1,8 @@
 __author__ = 'Udara'
 import requests
 import json
+from bs4 import BeautifulSoup
+import youtube_dl
 
 API_KEY = '5b905641511ae41eee7001a79e88775f'
 SECRET = 'cf537d2466ff4f57cabdf762db1bf980'
@@ -13,6 +15,25 @@ def get_song(artist):
 
     track_list = data['toptracks']['track']
     for track in track_list:
-        print(track['name'])
+        song = track['name']
+        print(artist+ ' ' + song)
+        get_youtube_link(artist + song)
 
-get_song('ladygaga')
+
+
+def get_youtube_link(artist_and_song):
+    source_code = requests.get('http://www.youtube.com/results?search_query=' + artist_and_song)
+    plain = source_code.text
+    soup = BeautifulSoup(plain)
+    links = soup.find_all('a', {'class': 'yt-uix-tile-link yt-ui-ellipsis yt-ui-ellipsis-2 yt-uix-sessionlink  \
+       spf-link '})
+    href = "https://www.youtube.com" + links[0].get('href')
+    download_link(href)
+
+
+def download_link(url):
+    ydl_opts = {}
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+get_youtube_link('50 Cent In Da Club')
